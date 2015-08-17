e87799028f77c80de2f32130438c5d542d0d17a9
Eric Brown <quadtree@gmail.com>
2015-08-17 01:14:21 -0700

    Started

---
+++ Source/HTML5PointLightIssue/HTML5PointLightIssueGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HTML5PointLightIssue.h"
+#include "HTML5PointLightIssueGameMode.h"
+
+
+
+
---
+++ Source/HTML5PointLightIssue/HTML5PointLightIssueGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "HTML5PointLightIssueGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HTML5POINTLIGHTISSUE_API AHTML5PointLightIssueGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
